7a7e769e8f38f6f647d209e6817744c22d38da89
Leobardo Delgado <leobardo.delgado@my.tccd.edu>
2022-08-24 21:36:11 -0500

    Hello world and section 1

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿//
+//Leo 
+//ITSE 1430 2022 fall 
+//Lab 1
+//
+
+namespace HelloWorld
+{
+    /*
+     *A Multiline commment
+     */
+    internal class Program
+    {
+        // A comment
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            //another block of code 
+        }
+    }
+}
