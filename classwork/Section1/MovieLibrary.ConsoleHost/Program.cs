56bbf0badc03da96f7298b3499e129bdcb89ba1b
Leobardo Delgado <leobardo.delgado@my.tccd.edu>
2022-08-29 21:05:35 -0500

    Started movie lib project

---
+++ classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,14 @@
+﻿//Movie Definition 
+string title = "";
+string description = "";
+int runLegnth = 0;
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+Console.WriteLine("Enter a title: ");
+Console.WriteLine("Enter an optional description: ");
+Console.WriteLine("Enter the run leghtn (in minutes): ");
+Console.WriteLine("Enter the relase year: ");
+Console.WriteLine("Enter the MPAA rating: ");
+Console.WriteLine("is this a classic?: ");
